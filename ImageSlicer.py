2203f1666000514e1bf50bdccdc3c2d9b5f7f8e2
Zud0taki <kosqzudotaki@gmail.com>
2021-11-04 12:15:13 +0100

    Added the boundary calc and list export

--- ImageSlicer.py
+++ ImageSlicer.py
@@ -3,8 +3,8 @@ from Input import *
 import cv2 as cv
 from BoundaryBoxCalc import *
 
-img_path = r"C:\Users\DLR_OS_Testbench\Desktop\TestImage"
-excel_input = readExcel(r"C:\Users\DLR_OS_Testbench\Desktop\TestExcel\SiemensTestCoords.xlsx")
+img_path = r"C:\Users\julia\Desktop\TestImage"
+excel_input = readExcel(r"C:\Users\julia\Desktop\TestExcel\SiemensTestCoords.xlsx")
 
 excel_info = excel_input['img_name']
 excel_list = excel_info.values.tolist()
@@ -34,11 +34,11 @@ for x in range(len(excel_list)):
     yc = y3 - (ycd / 2)
     yc = int(yc)
 
-    bounding_list = calcboundary(x1, x2, y1, y3)
-    boundaryul = bounding_list[0:2]
-    boundaryur = bounding_list[2:4]
-    boundarylr = bounding_list[4:6]
-    boundaryll = bounding_list[6:8]
+    # bounding_list = calcboundary(x1, x2, y1, y3)
+    # boundaryul = bounding_list[0:2]
+    # boundaryur = bounding_list[2:4]
+    # boundarylr = bounding_list[4:6]
+    # boundaryll = bounding_list[6:8]
 
     readingpath = r"" + (img_path) + "/" + img_name
     img = cv.imread(readingpath, -1)
@@ -63,6 +63,48 @@ for x in range(len(excel_list)):
         lyshift = ly
         ly = img_shape[0]
 
+    boundarylist = []
+
+    boundaryx1 = x1 - tx
+    #boundaryx1 = boundaryx1 - tx
+    boundaryx1 = int(boundaryx1)
+    boundarylist.append(boundaryx1)
+
+    boundaryy1 = y1 - ty
+    #boundaryy1 = boundaryy1 - ty
+    boundaryy1 = int(boundaryy1)
+    boundarylist.append(boundaryy1)
+
+    boundaryx2 = x2 - tx
+    boundaryx2 = int (boundaryx2)
+    boundarylist.append(boundaryx2)
+
+    boundaryy2 = y2 - ty
+    boundaryy2 = int(boundaryy2)
+    boundarylist.append(boundaryy2)
+
+    boundaryx3 = x3 - tx
+    boundaryx3 = int(boundaryx3)
+    boundarylist.append(boundaryx3)
+
+    boundaryy3 = y3 - ty
+    boundaryy3 = int(boundaryy3)
+    boundarylist.append(boundaryy3)
+
+    boundaryx4 = x4 - tx
+    boundaryx4 = int(boundaryx4)
+    boundarylist.append(boundaryx4)
+
+    boundaryy4 = y4 - ty
+    boundaryy4 = int(boundaryy4)
+    boundarylist.append(boundaryy4)
+
+    print(boundarylist)
+
+    with open("file.txt", "w") as output:
+        output.write(str(boundarylist))
+
+
     save_name = img_name.split(".")
     save_name = save_name[0]
     print(img.shape)
@@ -70,8 +112,10 @@ for x in range(len(excel_list)):
     print(original.shape)
     # first two values for rows - y values |||| second two values for columns - x values
     cropped = img[ty:ly, tx:lx]
-    cropped[boundaryul, boundarylr] = [255, 255, 255]
-    cropped[boundaryll, boundaryur] = [255, 255, 255]
+    cropped[boundaryx1, boundaryy1] = [0, 0, 255]
+    cropped[boundaryx2, boundaryy2] = [0, 0, 255]
+    cropped[boundaryx3, boundaryy3] = [0, 0, 255]
+    cropped[boundaryx4, boundaryy4] = [0, 0, 255]
     save = cv.imwrite(save_name+"_1.tif", cropped)
 
     # plt.imshow(img, aspect="auto")
