fd06a37cdb64784aaf21c7131119fd443c1eb1b5
Zud0taki <kosqzudotaki@gmail.com>
2021-11-26 16:27:56 +0100

    Added Export  for every Picture included testimages

--- DOTASlicer.py
+++ DOTASlicer.py
@@ -7,159 +7,157 @@ from BoundaryBoxCalc import *
 
 img_path = r"C:\Users\DLR_OS_Testbench\Desktop\tempImg"
 txt_path = r"C:\Users\DLR_OS_Testbench\Desktop\temptxt"
-name = "P0032"
 
-txt_file = open(txt_path + "/" + name +".txt", "r")
+
 coords_temp = []
+img_input = []
+txt_input = []
 label_count = 0
-for line in txt_file:
-    coords_temp = line.split()
-    x1 = coords_temp[0]
-    y1 = coords_temp[1]
-    x2 = coords_temp[2]
-    y2 = coords_temp[3]
-    x3 = coords_temp[4]
-    y3 = coords_temp[5]
-    x4 = coords_temp[6]
-    y4 = coords_temp[7]
+txt_counter = 0
 
-    xarray = np.array([x1, x2, x3, x4]).astype(np.int64)
-    xmax = xarray.max()
-    xmin = xarray.min()
+for txt in glob.glob(r"C:\Users\DLR_OS_Testbench\Desktop\temptxt\*"):
+    txt_input.append(txt)
+for img in glob.glob(r"C:\Users\DLR_OS_Testbench\Desktop\tempImg\*"):
+    img_input.append(img)
 
-    yarray = np.array([y1, y2, y3, y4]).astype(np.int64)
-    ymax = yarray.max()
-    ymin = yarray.min()
+for x in range(len(img_input)):
 
-    if x1 == xmin:
-        x1 = xmin
-    elif x1 == xmax:
-        x1 = xmax
-    else:
-        x1 = x1
-
-    if x2 == xmin:
-        x2 = xmin
-    elif x2 == xmax:
-        x2 = xmax
-    else:
-        x2 = x2
-
-    if x3 == xmin:
-        x3 = xmin
-    elif x3 == xmax:
-        x3 = xmax
-    else:
-        x3 = x3
-
-    if x4 == xmin:
-        x4 = xmin
-    elif x4 == xmax:
-        x4 = xmax
-    else:
-        x4 = x4
-
-    if y1 == ymin:
-        y1 = ymin
-    elif y1 == ymax:
-        y1 = ymax
-    else:
-        y1 = y1
-
-    if y2 == ymin:
-        y2 = ymin
-    elif y2 == ymax:
-        y2 = ymax
-    else:
-        y2 = y2
-
-    if y3 == ymin:
-        y3 = ymin
-    elif y3 == ymax:
-        y3 = ymax
-    else:
-        y3 = y3
-
-    if y4 == ymin:
-        y4 = ymin
-    elif y4 == ymax:
-        y4 = ymax
-    else:
-        y4 = y4
-    dummy = ""
-
-    xcd = xmax - xmin
-    xc = xmax - (xcd / 2)
-    xc = int(xc)
-    ycd = ymax - ymin
-    yc = ymax - (ycd / 2)
-    yc = int(yc)
-
-    readingpath = img_path + "/" + name+".png"
-    img = cv.imread(readingpath, -1)
-    img_shape = img.shape
-
-    # upper left boundary pixel
-    tx = xc - 208
-    if tx < 0:
-        txshift = tx
-        tx = 0
-    ty = yc - 208
-    if ty < 0:
-        tyshift = ty
-        ty = 0
-    # lower right boundary pixel
-    lx = tx + 416
-    if lx > img_shape[1]:
-        lxshift = lx
-        lx = img_shape[1]
-    ly = ty + 416
-    if ly > img_shape[0]:
-        lyshift = ly
-        ly = img_shape[0]
-
-    boundarylist = []
-
-    ucx = int(xmin) - tx
-    lcx = int(xmax) - tx
-
-    ucy = int(ymin) - ty
-    lcy = int(ymax) - ty
-
-    startpoint = [ucy, ucx]
-    endpoint = [lcy, lcx]
-    color = (0, 255, 0)
-    thickness = 1
-
-    xdistance = xmax - xmin
-    ydistance = ymax - ymin
-    xczeroed = xmin + (xdistance / 2)
-    yczeroed = ymin + (ydistance / 2)
-
-    export_list = [0, xczeroed, yczeroed, xdistance, ydistance]
-
-    save_name = [name]
+    txt_file = open(txt_input[x], "r")
+    save_name = txt_input[x]
+    save_name = save_name.split("\\temptxt")
     save_name = save_name[0]
-    save_name = r"C:\Users\DLR_OS_Testbench\Desktop\tempSave\ " + save_name
-    print(img.shape)
-    original = img[0:100, 0:100]
-    print(original.shape)
-    # first two values for rows - y values |||| second two values for columns - x values
-    cropped = img[ty:ly, tx:lx]
-    cropped = cv.rectangle(cropped, startpoint, endpoint, color, thickness)
 
-    txt_name = save_name
-    txt_name = txt_name+"_"+str(label_count)+ ".txt"
-    txtfile = open(txt_name, 'w')
-    for element in export_list:
-        txtfile.write(str(element) + " ")
-    txtfile.close()
-    save = cv.imwrite(save_name+"_"+str(label_count)+".tif", cropped)
+    img = cv.imread(img_input[x], -1)
+    img_shape = img.shape
+    for line in txt_file:
+        coords_temp = line.split()
+        x1 = coords_temp[0]
+        y1 = coords_temp[1]
+        x2 = coords_temp[2]
+        y2 = coords_temp[3]
+        x3 = coords_temp[4]
+        y3 = coords_temp[5]
+        x4 = coords_temp[6]
+        y4 = coords_temp[7]
+        label_name = coords_temp[8]
 
-    label_count += 1
+        xarray = np.array([x1, x2, x3, x4]).astype(np.int64)
+        xmax = xarray.max()
+        xmin = xarray.min()
 
-    # plt.imshow(img, aspect="auto")
-    # plt.show()
-    # plt.imshow(cropped, aspect="auto")
-    # plt.show()
-    # dummy = "j"
+        yarray = np.array([y1, y2, y3, y4]).astype(np.int64)
+        ymax = yarray.max()
+        ymin = yarray.min()
+
+        xcd = xmax - xmin
+        xc = xmax - (xcd / 2)
+        xc = int(xc)
+        ycd = ymax - ymin
+        yc = ymax - (ycd / 2)
+        yc = int(yc)
+
+        # readingpath = img_path + "/" + name+".png"
+        # img = cv.imread(readingpath, -1)
+        # img_shape = img.shape
+
+        # upper left boundary pixel
+        tx = xc - 208
+        if tx < 0:
+            txshift = tx
+            tx = 0
+        ty = yc - 208
+        if ty < 0:
+            tyshift = ty
+            ty = 0
+        # lower right boundary pixel
+        lx = tx + 416
+        if lx > img_shape[1]:
+            lxshift = lx
+            lx = img_shape[1]
+        ly = ty + 416
+        if ly > img_shape[0]:
+            lyshift = ly
+            ly = img_shape[0]
+
+        boundarylist = []
+
+        ucx = int(xmin) - tx
+        lcx = int(xmax) - tx
+
+        ucy = int(ymin) - ty
+        lcy = int(ymax) - ty
+
+        startpoint = [ucy, ucx]
+        endpoint = [lcy, lcx]
+        color = (0, 255, 0)
+        thickness = 1
+
+        xdistance = xmax - xmin
+        ydistance = ymax - ymin
+        xczeroed = xmin + (xdistance / 2)
+        yczeroed = ymin + (ydistance / 2)
+
+        label_number = 0
+        if(label_name == "plane"):
+            label_number = 0
+        elif(label_name == "ship"):
+            label_number = 1
+        elif (label_name == "storage"):
+            label_number = 2
+        elif (label_name == "tank"):
+            label_number = 3
+        elif (label_name == "baseball"):
+            label_number = 4
+        elif (label_name == "diamond"):
+            label_number = 5
+        elif (label_name == "tennis court"):
+            label_number = 6
+        elif (label_name == "basketball court"):
+            label_number = 7
+        elif (label_name == "ground track"):
+            label_number = 8
+        elif (label_name == "field"):
+            label_number = 9
+        elif (label_name == "harbor"):
+            label_number = 10
+        elif (label_name == "bridge"):
+            label_number = 11
+        elif (label_name == "large vehicle"):
+            label_number = 12
+        elif (label_name == "small vehicle"):
+            label_number = 13
+        elif (label_name == "helicopter"):
+            label_number = 14
+        elif (label_name == "roundabout"):
+            label_number = 15
+        elif (label_name == "soccer ball field"):
+            label_number = 16
+        elif (label_name == "swimming pool"):
+            label_number = 17
+        elif (label_name == "container crane"):
+            label_number = 18
+
+        export_list = [label_number, xczeroed, yczeroed, xdistance, ydistance]
+
+        print(img.shape)
+        original = img[0:100, 0:100]
+        print(original.shape)
+        # first two values for rows - y values |||| second two values for columns - x values
+        cropped = img[ty:ly, tx:lx]
+        cropped = cv.rectangle(cropped, startpoint, endpoint, color, thickness)
+
+        txt_name = save_name
+        txt_name = txt_name+"\\tempSave"+"_"+str(label_count)+ ".txt"
+        txtfile = open(txt_name, 'w')
+        for element in export_list:
+            txtfile.write(str(element) + " ")
+        txtfile.close()
+        save = cv.imwrite(save_name+"\\tempSave\\hallo"+"_"+str(label_count)+".tif", cropped)
+
+        label_count += 1
+    txt_counter += 1
+        # plt.imshow(img, aspect="auto")
+        # plt.show()
+        # plt.imshow(cropped, aspect="auto")
+        # plt.show()
+        # dummy = "j"
